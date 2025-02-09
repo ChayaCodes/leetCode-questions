6ad82f773d60abcb26ac455ee34e6519ead46774
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-09 16:34:21 +0200

    Time: 30 ms (37.28%) | Memory: 18.2 MB (20.72%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 
 class Solution(object):
     def groupAnagrams(self, strs):
@@ -8,16 +7,20 @@ class Solution(object):
         :param strs: List[str] - List of strings to be grouped
         :return: List[List[str]] - List of groups of anagrams
         """
-        freq_anagrams = defaultdict(list)
+        freq_anagrams = {}
 
         def generate_key(s):
             histo = [0] * 26
             for c in s:
                 histo[ord(c) - ord('a')] += 1
-            return str(histo)
+            return tuple(histo)
 
         for string in strs:
             key = generate_key(string)
-            freq_anagrams[key].append(string)
+            if key in freq_anagrams:
+                freq_anagrams[key].append(string)
+            else:
+                freq_anagrams[key] = [string]
 
         return list(freq_anagrams.values())
+        
