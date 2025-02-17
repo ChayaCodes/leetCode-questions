e5f958ef16aa0a01e4c05bca8a1a2886fe8eff23
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-17 18:36:54 +0200

    Time: 7 ms (89.42%) | Memory: 17 MB (85.94%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        def build_tree_rec(start, end):
+            if start >= end:
+                return None
+
+            node_val = preorder[self.preorder_index]
+            node = TreeNode(node_val)
+            self.preorder_index += 1
+            
+            inorder_index = inorder_map[node_val]
+            
+            # Build left and right subtrees
+            node.left = build_tree_rec(start, inorder_index)
+            node.right = build_tree_rec(inorder_index + 1, end)
+            
+            return node
+
+        self.preorder_index = 0
+        inorder_map = {val: idx for idx, val in enumerate(inorder)}  
+        return build_tree_rec(0, len(preorder))
