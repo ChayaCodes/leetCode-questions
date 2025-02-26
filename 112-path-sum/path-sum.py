975261a9570cae229364a2c7b3100c6641c66d64
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-26 22:38:23 +0200

    Time: 0 ms (100.00%) | Memory: 19 MB (23.74%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,11 +6,9 @@ class Solution:
 
         cur_sum += node.val
         
-        # Check if we are at a leaf node and the current sum matches the target sum
         if not node.left and not node.right:
             return cur_sum == targetSum
         
-        # Recursively check the left and right subtrees
         return (self._hasPathSumRec(node.left, targetSum, cur_sum) or 
                 self._hasPathSumRec(node.right, targetSum, cur_sum))
 
