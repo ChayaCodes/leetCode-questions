9977aa7a2a521f67faf55e738e54f762709211ce
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-27 10:48:20 +0200

    Time: 3 ms (90.59%) | Memory: 25 MB (58.77%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -4,21 +4,18 @@ class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
         self.stack = deque()
-        self.stack.append(root)
-        
+        self.append_leftmost_branch(root)
 
     def next(self) -> int:
-        cur = self.stack.pop()
-        cur_left = cur.left
-        if cur.left:
-            cur.left = None
-            self.stack.append(cur)
-            self.stack.append(cur_left)
-            return self.next()
-        if cur.right:
-            self.stack.append(cur.right)
-        return cur.val
-        
+        current = self.stack.pop()
+        if current.right:
+            self.append_leftmost_branch(current.right)
+        return current.val
 
     def hasNext(self) -> bool:
         return len(self.stack) > 0
+
+    def append_leftmost_branch(self, root: Optional[TreeNode]):
+        while root:
+            self.stack.append(root)
+            root = root.left
