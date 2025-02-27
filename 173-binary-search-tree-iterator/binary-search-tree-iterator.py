81205ce4d023b0e93bdf579295ba31b4de0675d3
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-27 10:48:05 +0200

    Time: 19 ms (5.51%) | Memory: 24 MB (99.52%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -1,22 +1,24 @@
 from collections import deque
 
 class BSTIterator:
+
     def __init__(self, root: Optional[TreeNode]):
-        self.stack = deque([root]) if root else deque()
+        self.stack = deque()
+        self.stack.append(root)
+        
 
     def next(self) -> int:
         cur = self.stack.pop()
-
+        cur_left = cur.left
         if cur.left:
+            cur.left = None
             self.stack.append(cur)
-            self.stack.append(cur.left)
-            cur.left = None  # כדי לא לחזור עליו שוב
+            self.stack.append(cur_left)
             return self.next()
-
         if cur.right:
             self.stack.append(cur.right)
-
         return cur.val
+        
 
     def hasNext(self) -> bool:
-        return bool(self.stack)
+        return len(self.stack) > 0
