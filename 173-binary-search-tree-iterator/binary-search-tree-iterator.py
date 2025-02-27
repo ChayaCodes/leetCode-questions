258c777a3a3f20ac9e572ffa3fa1e54d2ac9ae07
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-02-27 10:43:15 +0200

    Time: 7 ms (65.77%) | Memory: 24 MB (99.85%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -1,28 +1,22 @@
 from collections import deque
 
-
 class BSTIterator:
     def __init__(self, root: Optional[TreeNode]):
-        self.stack = deque()
-        self._push_left_branch(root)
-
-    def _push_left_branch(self, node: Optional[TreeNode]):
-        """ פונקציה עזר שמוסיפה את כל תת-העץ השמאלי למחסנית """
-        while node:
-            self.stack.append(node)
-            node = node.left
+        self.stack = deque([root]) if root else deque()
 
     def next(self) -> int:
-        """ מחזירה את הערך הבא בסדר ה- Inorder """
-        if not self.hasNext():
-            raise Exception("No more elements in BSTIterator")
-
         cur = self.stack.pop()
+
+        if cur.left:
+            self.stack.append(cur)
+            self.stack.append(cur.left)
+            cur.left = None  # כדי לא לחזור עליו שוב
+            return self.next()
+
         if cur.right:
-            self._push_left_branch(cur.right)
+            self.stack.append(cur.right)
 
         return cur.val
 
     def hasNext(self) -> bool:
-        """ מחזירה האם יש עוד אלמנטים באיטרטור """
         return bool(self.stack)
