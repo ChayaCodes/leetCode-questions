023a9045eedd2d8bb840090db9a2f6ef2354010b
chaya kremer <66823881+ChayaCodes@users.noreply.github.com>
2025-05-21 11:01:22 +0300

    Time: 21 ms (70.36%) | Memory: 23.3 MB (79.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,21 +1,19 @@
-from functools import reduce
-from operator import mul
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
         n = len(nums)
-        res = [1] * n
+
+        answer = [1] * n
         left = 1
         for i in range(n):
-            res[i] = left
+            answer[i] = left
             left *= nums[i]
         
         right = 1
         for i in range(n - 1, -1, -1):
-            res[i] *= right
+            answer[i] *= right
             right *= nums[i]
         
-        return res
+        return answer
+            
